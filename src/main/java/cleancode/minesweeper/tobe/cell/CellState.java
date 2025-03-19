e85fd831b01c93dfd231a03e5d42d9d6f2b82a65
Kwon JunWoo <top7578@hanwha.com>
2025-03-19 23:38:43 +0900

    상속과 조합

---
+++ src/main/java/cleancode/minesweeper/tobe/cell/CellState.java
@@ -0,0 +1,37 @@
+package cleancode.minesweeper.tobe.cell;
+
+public class CellState {
+
+    private boolean isFlagged;
+    private boolean isOpened;
+
+    public CellState(boolean isFlagged, boolean isOpened) {
+        this.isFlagged = isFlagged;
+        this.isOpened = isOpened;
+    }
+
+    public static CellState initialize() {
+        return new CellState(false, false);
+    }
+
+    public void flag() {
+        this.isFlagged = true;
+    }
+
+    public void open() {
+        this.isOpened = true;
+    }
+
+    public boolean isChecked() {
+        return this.isFlagged || this.isOpened;
+    }
+
+    public boolean isOpened() {
+        return this.isOpened;
+    }
+
+    public boolean isFlagged() {
+        return this.isFlagged;
+    }
+
+}
